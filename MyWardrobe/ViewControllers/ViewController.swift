da4cc0d5099688b70cf804866f89cc75360ecde9
elliott kung <elliottkung@elliotts-MacBook-Pro.local>
2020-09-21 15:14:44 -0400

    initial commit, setup skeleton template with main tab bar controller containing two VC

---
+++ MyWardrobe/ViewControllers/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  MyWardrobe
+//
+//  Created by elliott kung on 2020-09-21.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    let weatherVC = UINavigationController(rootViewController: WeatherViewController())
+    let closetVC = UINavigationController(rootViewController: ClosetViewController())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        weatherVC.title = "Todays Weather"
+        closetVC.title = "My Closet"
+        
+        self.setViewControllers([weatherVC, closetVC], animated: false)
+        guard let items = self.tabBar.items else {return}
+        let images = ["cloud", "star"]
+        for x in 0..<items.count{
+            items[x].image = UIImage(systemName: images[x])
+        }
+        
+    }
+
+
+}
+
