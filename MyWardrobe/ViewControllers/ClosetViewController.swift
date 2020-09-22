54690dda68b14763744fd45f5ce7cf9198ab7d8a
elliott kung <elliottkung@elliotts-MacBook-Pro.local>
2020-09-22 15:43:28 -0400

    added collectionview to ClosetVC and CustomCollectionViewCell file to create the template for items to be shown in a top scroll down manner

--- MyWardrobe/ViewControllers/ClosetViewController.swift
+++ MyWardrobe/ViewControllers/ClosetViewController.swift
@@ -7,24 +7,65 @@
 
 import UIKit
 
-class ClosetViewController: UIViewController {
+class ClosetViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource  {
 
+    private var collectionView: UICollectionView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
-
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(self.addItem))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(self.sortItems))
+    
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layoutCollectionView())
+        guard let collectionView = collectionView else { return }
+        
+        collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: CustomCollectionViewCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // TODO: call an capture image controller and allow user to take picture and add text to description
+    @objc func addItem(){
+        print("add")
     }
-    */
+    
+    // TODO: give user options to sort items based on certain descriptor categories
+    @objc func sortItems(){
+        print("filter")
+    }
 
+    // TODO: Add model for item with core data
+    
+    
+    // collection view
+    
+    func layoutCollectionView() -> UICollectionViewFlowLayout{
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
+        layout.itemSize = CGSize(width: (view.frame.size.width/3)-4, height: (view.frame.size.width/3)-4)
+        
+        return layout
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.identifier, for: indexPath) as! CustomCollectionViewCell
+        cell.configure(label: "Custom \(indexPath.row)")
+        
+        return cell
+    }
+    
+    // TODO: add VC for editing descriptors or items
+    // TODO: add delete method
+    
+    
 }
